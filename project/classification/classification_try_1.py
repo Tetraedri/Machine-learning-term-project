1019f2186a414a897b73e62a8bd9a9e8059f2ed4
Antti Luiro <anttiluiro@Anttis-MacBook-Pro.local>
2016-10-04 09:58:02 +0300

    python class

--- project/classification/classification_try_1.py
+++ project/classification/classification_try_1.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from numpy import genfromtxt
+import pandas as pd
 
-data = genfromtxt('classification_dataset_training.csv', delimiter=',')
+#df = pd.read_csv('classification_dataset_training.csv', delimiter=',', encoding="utf-8-sig")
 
-print data[1]
+my_data = np.genfromtxt('classification_dataset_training.csv', delimiter=',', dtype=None)
+positives = [a for a in my_data if a[len(my_data[0])] not in [0]]
+print(positives)
+result = [a for a in A if a not in subset_of_A]
+
+
+
+print(len(my_data[1][0]))
