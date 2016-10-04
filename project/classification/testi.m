6a7e26a2a29375db3031855b6ef76c7806e8acb9
Osama Abuzaid <usmiosama@hotmail.com>
2016-10-04 09:59:49 +0300

    eka tulos

--- project/classification/testi.m
+++ project/classification/testi.m
@@ -7,6 +7,8 @@ negcov = covariates(variates == 0, :);
 poscovsum = sum(poscov);
 negcovsum = sum(negcov);
 
+close all
+
 subplot(221)
 plot(1:length(poscovsum), poscovsum, 'kx')
 title('pos')
@@ -25,8 +27,21 @@ indices = [];
 norpos = poscovsum/length(variates(variates==1));
 norneg = negcovsum/length(variates(variates==0));
 
+subplot(222)
+hold on
+plot([1:length(norpos)], norpos-norneg)
+% plot([1:length(norpos)], norneg)
+% plot([1:length(norpos)], norpos)
+plot([1,length(norpos)], [0,0])
+figure
+hold on
+plot([1:length(norpos)], poscovsum-negcovsum)
+plot([1,length(norpos)], [0,0])
+
 for i = 1:length(poscovsum)
     if(norpos(i) < norneg(i))
         indices(end+1) = i;
     end
 end
+
+weights = norpos-norneg;
